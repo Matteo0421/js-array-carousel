741ade8184a001d0425aa56f01578bd6981d65da
Matteo0421 <matteo.carola99@gmail.com>
2024-02-26 15:35:06 +0100

    error

---
+++ README.md
@@ -0,0 +1,9 @@
+carosel carola-matteo
+===
+
+## CONSEGNA
+Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+
+## SVOLGIMENTO
+1. inserire tutte le img (percorsi) in un array
+2. inserire in item-slide tutte le img e dargli la classe hide
---
+++ js/script.js
@@ -0,0 +1,19 @@
+const itemsSlide = document.querySelector('.items-slide');
+const bottom = document.querySelector('.bottom');
+// const top = document.querySelector('.top');
+
+// 1
+const images = [
+  'img/01.webp',
+  '../consegna/img/02.webp',
+  '../consegna/img/03.webp',
+  '../consegna/img/04.webp',
+  '../consegna/img/05.webp',
+];
+
+// 2
+for(let i = 0; i < images.length; i++){
+  const img = images[i];
+  itemsSlide.innerHTML += `<img class="img hide" scr=" ${img} "> `
+  console.log(img);
+}
