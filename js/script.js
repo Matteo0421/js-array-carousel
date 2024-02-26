1539f932dc8df516583fc42366884c0c30e9919c
Matteo0421 <matteo.carola99@gmail.com>
2024-02-26 15:52:38 +0100

    img

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 const itemsSlide = document.querySelector('.items-slide');
 const bottom = document.querySelector('.bottom');
-const top = document.querySelector('.top');
+const sopra = document.querySelector('.sopra');
+
 
 // 1
 const images = [
-  'consegna/img/01.webp',
+  './consegna/img/01.webp',
   'consegna/img/02.webp',
   'consegna/img/03.webp',
   'consegna/img/04.webp',
@@ -16,5 +17,5 @@ for(let i = 0; i < images.length; i++){
   const img = images[i];
   itemsSlide.innerHTML += `<img class="img hide" scr=" ${img} "> `
   console.log(img);
-  console.log(itemsSlide);
+  // console.log(itemsSlide);
 }
